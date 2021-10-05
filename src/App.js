e620de5808093f2a14d1c7231bbc836ebb37271b
Yasmine De Wolf <yasmine.de.wolf@student.ehb.be>
2021-10-05 15:37:40 +0200

    cleaning up code + added solution screenshot

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import "./styles/_main.scss";
-import Header from "./components/Header"
+import "./styles/main.scss";
+import Header from "./components/Header";
 import Home from "./pages/home";
 import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Home />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
